2b74c774f8eb1754146eb37c8f998dd6e9a1a681
Aryaman Tiwary <87538625+Aryaman0809@users.noreply.github.com>
2022-12-02 18:17:15 +0530

    Add files via upload

---
+++ lib/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color(0xFFFF3378);
+Color secondary = Color(0xffDA0959);
+Color black = Color(0xFF000000);
+Color white = Color(0xFFFFFFFF);
+Color grey = Colors.grey;
+Color red = Color(0xFFec5766);
+Color green = Color(0xFF43aa8b);
+Color blue = Color(0xFF28c2ff);
+
+List<List<Color>> myColors = [
+  [Color(0xff008F7A), Color(0xff7DE197)],
+  [Color(0xff671ac5), Color(0xff7e92ee)],
+  [Color(0xff155062), Color(0xff42aae8)],
+  [Color(0xffDA0959), Color(0xffF47DCB)]
+];
+
